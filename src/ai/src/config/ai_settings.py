9ced263b93adeaabab65d39764e98e5d8239c519
philexlive <philex.live@outlook.com>
2025-12-25 04:06:01 +0000

    Necessary files removed from .gitignore

---
+++ src/ai/src/config/ai_settings.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+# Provides environmental variable for an ai service
+class AISettings(BaseSettings):
+    model_name: str = Field(validation_alias="MODEL")
+    base_url: Optional[str] = Field(None, validation_alias="BASE_URL")
+    provider_name: Optional[str] = Field(None, validation_alias="PROVIDER")
+    hf_api_key: Optional[str] = Field(None, validation_alias="HF_TOKEN")
+    mistral_api_key: Optional[str] = Field(None, validation_alias="MISTAL_API_KEY")
+
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8", 
+        extra='ignore')
+
+
+ai_settings = AISettings()
